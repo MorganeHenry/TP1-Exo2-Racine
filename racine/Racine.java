d99c9b514a00bf663cd6fc7d0d65724d77a3bd4e
MorganeHenry <44085609+MorganeHenry@users.noreply.github.com>
2019-02-04 11:45:02 +0100

    Add files via upload

---
+++ racine/Racine.java
@@ -0,0 +1,43 @@
+package racine;
+
+
+public class Racine {
+
+    private double precision;
+
+    Racine(double precision) {
+        this.precision = precision;
+    }
+
+    double racine(double operande) throws NegativeException {
+        if (operande < 0) {
+            throw new NegativeException("Le nombre entier est négatif");
+        }
+
+
+        double solution, carre, a, b;
+
+
+        a = 0;
+        b = operande;
+        solution = (a + b) / 2;
+        while (b - a > precision) {
+            carre = solution * solution;
+            if (carre > operande) {
+                b = solution;
+            } else {
+                a = solution;
+            }
+            solution = (a + b) / 2;
+        }
+        return solution;
+    }
+
+    double getPrecision() {
+        return precision;
+    }
+
+    void setPrecision(double precision) {
+        this.precision = precision;
+    }
+}
